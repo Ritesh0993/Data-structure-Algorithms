cc8d53d2cbbd49b1e0982761e201d42343dd577c
riteshPatil <ritesh93patil@outlook.com>
2024-06-14 17:33:02 +0530

    Pattern Programs

--- src/java/patterns/PatternPrograms.java
+++ src/java/patterns/PatternPrograms.java
@@ -1,5 +1,7 @@
 package patterns;
 
+import java.sql.SQLOutput;
+
 public class PatternPrograms {
 
 
@@ -75,14 +77,72 @@ public class PatternPrograms {
 
     }
 
+    /**
+     *  Number increasing Triangle
+     *         1
+     *         12
+     *         123
+     *         1234
+     * @param
+     */
+
+
+    public static void numberIncreasingPyramid(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     *  Number Triangle
+     *         1
+     *         22
+     *         333
+     *         4444
+     * @param
+     */
+
+
+    public static void numberTriangle(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     *  Number increasing reverse Triangle
+     *         12345
+     *         1234
+     *         123
+     *         12
+     *         1
+     * @param
+     */
+
+
+    public static void numberIncreasingReversePyramid(int n){
+
+
+
+
+    }
+
 
 
 
     public static void main(String[] args) {
         //squareFillPattern(5);
         //rightHalfPyramid(3);
-        reverseRightHalfPyramidPattern(6);
+        //reverseRightHalfPyramidPattern(6);
+       // numberIncreasingPyramid(5);
+        //numberTriangle(10);
+        numberIncreasingReversePyramid(10);
     }
-
 
 }
