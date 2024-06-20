4d501a1082723e35bc0a581ed619d5367d1444fc
riteshPatil <ritesh93patil@outlook.com>
2024-06-20 10:54:19 +0530

    Pattern Programs

--- src/java/patterns/PatternPrograms.java
+++ src/java/patterns/PatternPrograms.java
@@ -6,11 +6,11 @@ public class PatternPrograms {
 
 
     /**
-     *  Print the Square Fill Pattern
-     *
-     *       ****
-     *       ****
-     *       ****
+     * Print the Square Fill Pattern
+     * <p>
+     * ****
+     * ****
+     * ****
      *
      * @param n
      */
@@ -25,22 +25,22 @@ public class PatternPrograms {
     }
 
 
-    /**  Right HalfPyramid
-     *
-     *          *
-     *          **
-     *          ***
-     *          ****
-     *
+    /**
+     * Right HalfPyramid
+     * <p>
+     * *
+     * **
+     * ***
+     * ****
      *
      * @param n
      */
 
-    public static void rightHalfPyramid(int n){
+    public static void rightHalfPyramid(int n) {
 
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
 
-            for(int j=0;j<=i;j++){
+            for (int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -50,21 +50,22 @@ public class PatternPrograms {
 
 
     /**
-     *  Left Half Pyramid
-     *            ******
-     *            *****
-     *            ****
-     *            ***
-     *            **
-     *            *
+     * Left Half Pyramid
+     * ******
+     * *****
+     * ****
+     * ***
+     * **
+     * *
+     *
      * @param
      */
 
 
     public static void reverseRightHalfPyramidPattern(int n) {
 
-        for(int i=1;i<=n;i++){
-            for(int j =0;j<n-i+1;j++){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n - i + 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -74,50 +75,53 @@ public class PatternPrograms {
     }
 
     /**
-     *  Number increasing Triangle
-     *         1
-     *         12
-     *         123
-     *         1234
+     * Number increasing Triangle
+     * 1
+     * 12
+     * 123
+     * 1234
+     *
      * @param
      */
 
 
-    public static void numberIncreasingPyramid(int n){
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(j+" ");
+    public static void numberIncreasingPyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
     }
 
     /**
-     *  Number Triangle
-     *         1
-     *         22
-     *         333
-     *         4444
+     * Number Triangle
+     * 1
+     * 22
+     * 333
+     * 4444
+     *
      * @param
      */
 
 
-    public static void numberTriangle(int n){
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(i+" ");
+    public static void numberTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
             }
             System.out.println();
         }
     }
 
     /**
-     *  Number increasing reverse Triangle
-     *         12345
-     *         1234
-     *         123
-     *         12
-     *         1
+     * Number increasing reverse Triangle
+     * 12345
+     * 1234
+     * 123
+     * 12
+     * 1
+     *
      * @param
      */
 
@@ -132,15 +136,116 @@ public class PatternPrograms {
         }
     }
 
+    /**     Triangle Star Pattern
+     *          *
+     *         ***
+     *        *****
+     *       *******
+     *      *********
+     *     ***********
+     *    *************
+     *   ***************
+     *  *****************
+     * *******************
+     * @param n
+     */
+
+    public static void triangleStarPattern(int n) {
+        for (int i = 0; i < n; i++) {
+
+            //space
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            //star
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+
+            //space
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void triangleStarReversePattern(int n) {
+        for (int i = 0; i < n; i++) {
+
+            //space
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            //star
+            for (int j = 0; j < 2*n-(2*i+1); j++) {
+                System.out.print("*");
+            }
+
+            //space
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    /**
+     *   Star Diamond Pattern
+     * @param
+     */
+
+    public static void startDiamondPattern(int n){
+        for (int i = 0; i < n; i++) {
+
+            //space
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            //star
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+
+            //space
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            //space
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            //star
+            for (int j = 0; j < 2*n-(2*i+1); j++) {
+                System.out.print("*");
+            }
+
+            //space
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
 
 
     public static void main(String[] args) {
         //squareFillPattern(5);
         //rightHalfPyramid(3);
-        reverseRightHalfPyramidPattern(6);
-       // numberIncreasingPyramid(5);
+        //reverseRightHalfPyramidPattern(6);
+        // numberIncreasingPyramid(5);
         //numberTriangle(10);
-        numberIncreasingReversePyramid(10);
+        // numberIncreasingReversePyramid(10);
+        //triangleStarPattern(10);
+       // triangleStarReversePattern(10);
+        startDiamondPattern(5);
     }
 
 }
